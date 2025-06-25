2cc46c3cd8052a75ee463e650e0fd62c8b5f82a3
kalenoronha <kalenoronha@outlook.com.pe>
2025-06-25 13:17:18 -0500

    intento 3

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,3 +1,4 @@
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 
 export async function fetch3DS(body) {
@@ -29,7 +30,8 @@ export async function fetch3DS(body) {
 }
 
 export async function fetchACS(body) {
-  const res = await fetch(`http://localhost:4000/api/es/scm`, {
+    const res = await fetch(`${API_BASE_URL}/api/es/scm`, {
+ 
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(body),
